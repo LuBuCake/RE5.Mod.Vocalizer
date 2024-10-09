4ef1c4d11154c1907857d6a1a6593033df283e61
Lucas <lubucake@outlook.com>
2024-10-09 12:31:44 -0300

    added 1 more group

--- RE5.Mod.Vocalizer/RE5.Mod.Vocalizer/dllmain.cpp
+++ RE5.Mod.Vocalizer/RE5.Mod.Vocalizer/dllmain.cpp
@@ -7,6 +7,34 @@ using namespace std;
 
 bool PerforminActionReponse = false;
 
+// Keymapping
+
+char g_WATCHOUT = VK_NUMPAD0;
+char g_SHIT = VK_NUMPAD1;
+char g_GOAHEAD = VK_NUMPAD2;
+char g_GOODWORK = VK_NUMPAD3;
+char g_YOUTAKE = VK_NUMPAD4;
+char g_CALLPARTNER = VK_NUMPAD5;
+char g_FANOUT = VK_NUMPAD6;
+char g_TAKECARE = VK_NUMPAD7;
+char g_FOLLOWME = VK_NUMPAD8;
+char g_IOWEYOU = VK_NUMPAD9;
+char g_LEAVETOME = VK_DECIMAL;
+char g_BECAREFUL = VK_NEXT;
+char g_IMCOMING = VK_PRIOR;
+char g_SAYYES = VK_ADD;
+char g_SAYNO = VK_SUBTRACT;
+char g_CALLPARTNERSOFT = VK_DIVIDE;
+char g_CALLFORHELP = VK_MULTIPLY;
+char g_CANTCARRY = VK_INSERT;
+char g_NICESHOT = VK_DELETE;
+char g_DYING = VK_HOME;
+char g_PARTNERDEAD = VK_END;
+char g_GO = VK_F1;
+char g_WAIT = VK_F2;
+char g_GIVEITEM = VK_F4;
+char g_GIVEMEITEM = VK_F3;
+
 // Custom Groups
 
 unsigned short SHIT[3] = { 72, 73, 0 };
@@ -31,9 +59,6 @@ unsigned short CALLFORHELP[3] = { 43, 44, 0 };
 unsigned short DYING[3] = { 45, 46, 0 };
 unsigned short LEAVETOME[1] = { 21 };
 unsigned short GIVEMEITEM[1] = { 80 };
-
-// Partner Commands
-
 unsigned short GO[1] = { 16 };
 unsigned short WAIT[1] = { 27 };
 
@@ -140,81 +165,81 @@ DWORD WINAPI MainThread(LPVOID param)
 			PARTNERDEAD[0] = 0;
 		}
 
-		if (KeyPressed(VK_NUMPAD0) != 0)
+		if (KeyPressed(g_WATCHOUT) != 0)
 		{
 			Game::PlaySpeech(79, LocalPlayer);
 		}
-		else if (KeyPressed(VK_NUMPAD1) != 0)
+		else if (KeyPressed(g_SHIT) != 0)
 		{
 			Game::PlaySpeech(SHIT[SHIT[2]], LocalPlayer);
 			SHIT[2]++;
 			SHIT[2] = SHIT[2] > 1 ? 0 : SHIT[2];
 		}
-		else if (KeyPressed(VK_NUMPAD2) != 0)
+		else if (KeyPressed(g_GOAHEAD) != 0)
 		{
 			Game::PlaySpeech(GOAHEAD[GOAHEAD[2]], LocalPlayer);
 			GOAHEAD[2]++;
 			GOAHEAD[2] = GOAHEAD[2] > 1 ? 0 : GOAHEAD[2];
 		}
-		else if (KeyPressed(VK_NUMPAD3) != 0)
+		else if (KeyPressed(g_GOODWORK) != 0)
 		{
 			Game::PlaySpeech(GOODWORK[GOODWORK[3]], LocalPlayer);
 			GOODWORK[3]++;
 			GOODWORK[3] = GOODWORK[3] > 2 ? 0 : GOODWORK[3];
 		}
-		else if (KeyPressed(VK_NUMPAD4) != 0)
+		else if (KeyPressed(g_YOUTAKE) != 0)
 		{
 			Game::PlaySpeech(YOUTAKE[YOUTAKE[3]], LocalPlayer);
 			YOUTAKE[3]++;
 			YOUTAKE[3] = YOUTAKE[3] > 2 ? 0 : YOUTAKE[3];
 		}
-		else if (KeyPressed(VK_NUMPAD5) != 0)
+		else if (KeyPressed(g_CALLPARTNER) != 0)
 		{
 			Game::PlaySpeech(CALLPARTNER[CALLPARTNER[3]], LocalPlayer);
 			CALLPARTNER[3]++;
 			CALLPARTNER[3] = CALLPARTNER[3] > 2 ? 0 : CALLPARTNER[3];
 		}
-		else if (KeyPressed(VK_NUMPAD6) != 0)
+		else if (KeyPressed(g_FANOUT) != 0)
 		{
 			Game::PlaySpeech(FANOUT[FANOUT[4]], LocalPlayer);
 			FANOUT[4]++;
 			FANOUT[4] = FANOUT[4] > 3 ? 0 : FANOUT[4];
 		}
-		else if (KeyPressed(VK_NUMPAD7) != 0)
+		else if (KeyPressed(g_TAKECARE) != 0)
 		{
 			Game::PlaySpeech(TAKECARE[TAKECARE[2]], LocalPlayer);
 			TAKECARE[2]++;
 			TAKECARE[2] = TAKECARE[2] > 1 ? 0 : TAKECARE[2];
 		}
-		else if (KeyPressed(VK_NUMPAD8) != 0)
+		else if (KeyPressed(g_FOLLOWME) != 0)
 		{
 			Game::PlaySpeech(FOLLOWME[FOLLOWME[2]], LocalPlayer);
 			FOLLOWME[2]++;
 			FOLLOWME[2] = FOLLOWME[2] > 1 ? 0 : FOLLOWME[2];
 		}
-		else if (KeyPressed(VK_NUMPAD9) != 0)
+		else if (KeyPressed(g_IOWEYOU) != 0)
 		{
 			Game::PlaySpeech(IOWEYOU[IOWEYOU[3]], LocalPlayer);
 			IOWEYOU[3]++;
 			IOWEYOU[3] = IOWEYOU[3] > 2 ? 0 : IOWEYOU[3];
 		}
-		else if (KeyPressed(VK_SUBTRACT) != 0)
+		else if (KeyPressed(g_SAYNO) != 0)
 		{
 			Game::PlaySpeech(SAYNO[SAYNO[3]], LocalPlayer);
 			SAYNO[3]++;
 			SAYNO[3] = SAYNO[3] > 2 ? 0 : SAYNO[3];
 		}
-		else if (KeyPressed(VK_ADD) != 0)
+		else if (KeyPressed(g_SAYYES) != 0)
 		{
 			Game::PlaySpeech(SAYYES[SAYYES[2]], LocalPlayer);
 			SAYYES[2]++;
 			SAYYES[2] = SAYYES[2] > 1 ? 0 : SAYYES[2];
 		}
-		else if (KeyPressed(VK_DECIMAL) != 0)
+		else if (KeyPressed(g_LEAVETOME) != 0)
 		{
 			Game::PlaySpeech(LEAVETOME[0], LocalPlayer);
 		}
-		else if (KeyPressed(VK_END) != 0)
+		else if (KeyPressed(g_PARTNERDEAD) != 0)
 		{
 			int PlayerChar = Game::GetCharacter(LocalPlayerIndex);
 
@@ -226,55 +251,55 @@ DWORD WINAPI MainThread(LPVOID param)
 			else
 				Game::PlaySpeech(PARTNERDEAD[0], LocalPlayer);
 		}
-		else if (KeyPressed(VK_DELETE) != 0)
+		else if (KeyPressed(g_NICESHOT) != 0)
 		{
 			Game::PlaySpeech(NICESHOT[NICESHOT[3]], LocalPlayer);
 			NICESHOT[3]++;
 			NICESHOT[3] = NICESHOT[3] > 2 ? 0 : NICESHOT[3];
 		}
-		else if (KeyPressed(VK_HOME) != 0)
+		else if (KeyPressed(g_DYING) != 0)
 		{
 			Game::PlaySpeech(DYING[DYING[2]], LocalPlayer);
 			DYING[2]++;
 			DYING[2] = DYING[2] > 1 ? 0 : DYING[2];
 		}
-		else if (KeyPressed(VK_INSERT) != 0)
+		else if (KeyPressed(g_CANTCARRY) != 0)
 		{
 			Game::PlaySpeech(CANTCARRY[CANTCARRY[3]], LocalPlayer);
 			CANTCARRY[3]++;
 			CANTCARRY[3] = CANTCARRY[3] > 2 ? 0 : CANTCARRY[3];
 		}
-		else if (KeyPressed(VK_PRIOR) != 0)
+		else if (KeyPressed(g_IMCOMING) != 0)
 		{
 			Game::PlaySpeech(IMCOMING[IMCOMING[2]], LocalPlayer);
 			IMCOMING[2]++;
 			IMCOMING[2] = IMCOMING[2] > 1 ? 0 : IMCOMING[2];
 		}
-		else if (KeyPressed(VK_NEXT) != 0)
+		else if (KeyPressed(g_BECAREFUL) != 0)
 		{
 			Game::PlaySpeech(BECAREFUL[BECAREFUL[3]], LocalPlayer);
 			BECAREFUL[3]++;
 			BECAREFUL[3] = BECAREFUL[3] > 2 ? 0 : BECAREFUL[3];
 		}
-		else if (KeyPressed(VK_MULTIPLY) != 0)
+		else if (KeyPressed(g_CALLFORHELP) != 0)
 		{
 			Game::PlaySpeech(CALLFORHELP[CALLFORHELP[2]], LocalPlayer);
 			CALLFORHELP[2]++;
 			CALLFORHELP[2] = CALLFORHELP[2] > 1 ? 0 : CALLFORHELP[2];
 		}
-		else if (KeyPressed(VK_DIVIDE) != 0)
+		else if (KeyPressed(g_CALLPARTNERSOFT) != 0)
 		{
 			Game::PlaySpeech(CALLPARTNERSOFT[0], LocalPlayer);
 		}
-		else if (KeyPressed(VK_F1) != 0)
+		else if (KeyPressed(g_GO) != 0)
 		{
 			Game::PlaySpeech(GO[0], LocalPlayer);
 		}
-		else if (KeyPressed(VK_F2) != 0)
+		else if (KeyPressed(g_WAIT) != 0)
 		{
 			Game::PlaySpeech(WAIT[0], LocalPlayer);
 		}
-		else if (KeyPressed(VK_F3) != 0)
+		else if (KeyPressed(g_GIVEMEITEM) != 0)
 		{
 			if (Partner != 0)
 			{
@@ -383,6 +408,12 @@ DWORD WINAPI MainThread(LPVOID param)
 				GIVEMEITEM[0] = 88;
 
 			Game::PlaySpeech(GIVEMEITEM[0], LocalPlayer);
+		}
+		else if (KeyPressed(g_GIVEITEM) != 0)
+		{
+			Game::PlaySpeech(GIVEITEM[GIVEITEM[2]], LocalPlayer);
+			GIVEITEM[2]++;
+			GIVEITEM[2] = GIVEITEM[2] > 1 ? 0 : GIVEITEM[2];
 		}
 
 		// Action response
